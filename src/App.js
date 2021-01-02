6159170ae64ec61f5663ab6756d57f53c7674b25
RickyAnchores <ricky23anchores@gmail.com>
2021-01-02 17:18:31 +0000

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hey Ricky</h1>
       <p>This is your Portfolio</p>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header(){
+    return(
+    <div className="Header">
+        <div className="logo">
+            <a href=""><h1>Ricky A Dev</h1></a>
+        </div>
+        <div className="nav">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Contacts</a></li>
+            </ul>
+        </div>
+    </div>
+    )};
+
