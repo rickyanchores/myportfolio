0f856f9388c5b8cf0fc9ae9a8505dc68f7c01958
RickyAnchores <ricky23anchores@gmail.com>
2021-01-02 18:45:01 +0000

    added Showcase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Showcase from "./components/Showcase";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Hey Ricky</h1>
-      <p>This is your Portfolio</p>
+      <Showcase />
     </div>
   );
 }
---
+++ src/components/Showcase.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Showcase(){
+    return(
+        <div className="Showcase">
+            <h3>Hi, my name is</h3>
+            <h1>Ricky A</h1>
+            <h3>Front-End Web Developer</h3>
+        </div>
+    )
+}
