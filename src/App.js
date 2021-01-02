cc0b81cedc854bcc686a8eedc42b77b70e300498
RickyAnchores <ricky23anchores@gmail.com>
2021-01-02 17:04:58 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hey Ricky</h1>
+      <p>This is your Portfolio</p>
+    </div>
+  );
+}
+
+
