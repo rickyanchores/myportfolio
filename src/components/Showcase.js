fa879cc5291257b67039ba3f5fe7ba191ff2f72a
RickyAnchores <ricky23anchores@gmail.com>
2021-01-09 10:26:11 +0000

    added photo and google font

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Showcase(){
     return(
         <div className="Showcase">
+            <img src="https://instagram.fbhx3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/131250653_221093666206428_5333543334734715967_n.jpg?_nc_ht=instagram.fbhx3-1.fna.fbcdn.net&_nc_ohc=U3JFSfa6xAoAX9maGNS&tp=1&oh=de6dfd09005246586c097b700d158f8a&oe=60213D01" alt=""/>
             <h3>Hi, my name is</h3>
             <h1>Ricky A</h1>
             <h3>Front-End Web Developer</h3>
