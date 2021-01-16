f7fcfb899fe0abb8567eb6d298f9834640b5e699
RickyAnchores <ricky23anchores@gmail.com>
2021-01-16 12:01:17 +0000

    added email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ export default function Footer(){
                 <div className="logo"><h1>Ricky A Dev</h1></div>
                 <div className="nav-media">
                     <ul>
+                        <li><a href="https://mail.google.com/mail/rikidev4@gmail.com" target="blank">rikidev4@gmail.com</a></li>
                         <li><a href="https://www.linkedin.com/in/ricky-anchores-653a15140/" target="blank">LINKEDIN</a></li>
                         <li><a href="https://github.com/rickyanchores" target="blank">GITHUB</a></li>
                         <li><a href="https://www.instagram.com/rikid_ev/" target="blank">INSTAGRAM</a></li>
