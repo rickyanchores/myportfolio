67ba2fe6fca20fec083f211603c016676935136f
RickyAnchores <ricky23anchores@gmail.com>
2021-02-02 23:20:12 +0000

    added Kahoii Project link

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react'
-import NET from "vanta/dist/vanta.net.min"
+import React, { PureComponent } from 'react';
+import NET from "vanta/dist/vanta.net.min";
 
 
 export default class Background extends PureComponent {
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,9 +7,9 @@ export default function Projects(){
             <div className="Cards">
                 <div className="card">
                     <img src="" alt=""/>
-                    <h3>Title</h3>
+                    <h3>Kahoii</h3>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A ipsa commodi deleniti hic laboriosam praesentium?</p>
-                    <button><a href="">Read more</a></button>
+                    <button><a href="https://rickyanchores.github.io/Kahoii/" target="_blank">Read more</a></button>
                 </div>
                 <div className="card">
                     <img src="" alt=""/>
