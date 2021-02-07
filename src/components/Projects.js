3fa8cc4e4da88e77b43d81390e144ae9c1cad79c
RickyAnchores <ricky23anchores@gmail.com>
2021-02-07 15:36:53 +0000

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,17 +14,17 @@ export default function Projects(){
                 </div>
                 <div className="card">
                     <img className="imgB" src="" alt=""/>
-                    <h3>Title</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A ipsa commodi deleniti hic laboriosam praesentium?</p>
-                    <button><a href="">LIVE</a></button>
-                    <button><a href="" target="_blank">CODE</a></button>
+                    <h3>Zenkana</h3>
+                    <p>React Website with minimalist design (my custom TEMPLATE)</p>
+                    <button><a href="https://rickyanchores.github.io/zenkana/">LIVE</a></button>
+                    <button><a href="https://github.com/rickyanchores/zenkana" target="_blank">CODE</a></button>
                 </div>
                 <div className="card">
                     <img className="imgC" src="" alt=""/>
-                    <h3>Title</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A ipsa commodi deleniti hic laboriosam praesentium?</p>
-                    <button><a href="">LIVE</a></button>
-                    <button><a href="" target="_blank">CODE</a></button>
+                    <h3>Rickolor</h3>
+                    <p>Landing Page with personal design using Adobe XD</p>
+                    <button><a href="https://rickyanchores.github.io/rickolor/">LIVE</a></button>
+                    <button><a href="https://github.com/rickyanchores/rickolor" target="_blank">CODE</a></button>
                 </div>
             </div>
         </div>
