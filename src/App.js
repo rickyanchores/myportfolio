ef5dc3535077d4be62694153dadd3c3e540bc181
RickyAnchores <ricky23anchores@gmail.com>
2021-02-07 16:08:17 +0000

    added react-animated-cursor

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Showcase from "./components/Showcase";
 import Background from "./components/Background";
-
+import AnimatedCursor from "react-animated-cursor";
 export default function App() {
   return (
     <div className="App">
+      <AnimatedCursor
+        innerSize={12}
+        outerSize={32}
+        color='0, 128, 128'
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+       />
       <Background />
       <Header />
       <Showcase />
