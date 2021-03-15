543e37d51c0cf0dbe63b4f584a47d80d866a0449
RickyAnchores <ricky23anchores@gmail.com>
2021-03-15 18:06:57 +0000

    update me pic

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Showcase(){
     return(
         <div className="Showcase" id="Showcase">
-            <img src="https://scontent.fltn2-1.fna.fbcdn.net/v/t1.0-9/96103463_10217035850792739_5320817155158048768_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=174925&_nc_ohc=TkWpv7FAT8MAX_PSgFQ&_nc_ht=scontent.fltn2-1.fna&oh=7b3db5a04eab25a2fddf6b03a4fd60a5&oe=6074F2E3" alt=""/>
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQEQRcsZ24KGwg/profile-displayphoto-shrink_800_800/0/1516955662523?e=1621468800&v=beta&t=lVsi5-bhdD8BeksFk9Uz2K_Jed3gBaO48QsBIq4QfR4" alt="me"/>
             <h3>Hi, my name is</h3>
             <h1>Ricky A</h1>
             <h3>Front-End Web Developer</h3>
