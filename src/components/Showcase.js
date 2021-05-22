63cf2131137f69168311bd7f45066c488a7d294e
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2021-05-22 19:30:17 +0100

    Update Showcase.js
    updated img

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Showcase(){
     return(
         <div className="Showcase" id="Showcase">
-            <img src="https://media-exp1.licdn.com/dms/image/C5603AQEQRcsZ24KGwg/profile-displayphoto-shrink_800_800/0/1516955662523?e=1621468800&v=beta&t=lVsi5-bhdD8BeksFk9Uz2K_Jed3gBaO48QsBIq4QfR4" alt="me"/>
+            <img src="https://scontent.fltn2-1.fna.fbcdn.net/v/t1.6435-9/96103463_10217035850792739_5320817155158048768_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=174925&_nc_ohc=S0akT5oEA2YAX9AmMs4&_nc_ht=scontent.fltn2-1.fna&oh=42b55bc02507c3ef02c74eb186cd0f98&oe=60CD6482" alt="me"/>
             <h3>Hi, my name is</h3>
             <h1>Ricky A</h1>
             <h3>Front-End Web Developer</h3>
