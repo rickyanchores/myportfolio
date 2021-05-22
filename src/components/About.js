dfc1168f2116d8c5a0e251bc3232135f3f124a14
RickyAnchores <ricky23anchores@gmail.com>
2021-05-22 21:19:20 +0100

    updated showcase and about

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About(){
     return(
         <div className="About" id="About">
             <div className="img-about">
-                <img src="https://scontent.fltn2-1.fna.fbcdn.net/v/t1.0-9/45093174_10212921473015866_8798329360139419648_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=a4a2d7&_nc_ohc=kv4wY_AH6NsAX8WAFRg&_nc_ht=scontent.fltn2-1.fna&oh=68ff2e3f77c52c1fcaceb979ecd7bf70&oe=6071E05D" alt=""/>
+                <img src="https://scontent.fltn2-1.fna.fbcdn.net/v/t1.6435-9/45093174_10212921473015866_8798329360139419648_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=a4a2d7&_nc_ohc=jZyBZ7b39LQAX_Yy_ap&_nc_ht=scontent.fltn2-1.fna&oh=236bbc3d7ba56f9f0eaff7972e26f51b&oe=60CF5AC0"alt=""/>
             </div>
             <div className="txt-about">
                 <h3>About</h3>
